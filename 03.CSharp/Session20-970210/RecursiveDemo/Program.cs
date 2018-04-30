eed95c55a189b0b0164970590cc7447f1f1f2ac1
Farhang Vafa <farhangv@ymail.com>
2018-04-30 13:31:10 +0430

    Session20-970210 Second Commit

---
+++ 03.CSharp/Session20-970210/RecursiveDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(3));
+            Console.ReadKey();
+        }
+
+        static int Factorial(int num)
+        {
+            if (num == 1)
+                return 1;
+            else
+                return num * Factorial(--num);
+        }
+    }
+}
