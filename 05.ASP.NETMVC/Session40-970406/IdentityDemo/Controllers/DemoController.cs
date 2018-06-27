77045b589f0a62c316e08199e2b2703ee05394a0
Farhang Vafa <farhangv@ymail.com>
2018-06-27 13:11:57 +0430

    Session40-970406

---
+++ 05.ASP.NETMVC/Session40-970406/IdentityDemo/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return Content("Index");
+        }
+        [Authorize]
+        public ActionResult Details()
+        {
+            return Content("Details");
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit()
+        {
+            return Content("Edit");
+        }
+        [Authorize(Users = "farhangv@gmail.com")]
+        public ActionResult Delete()
+        {
+            return Content("Delete");
+        }
+    }
+}
